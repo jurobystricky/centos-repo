a123766cfb1197427a0a26cc9c4158f69901c871
Lu Ken <ken.lu@intel.com>
2020-04-05 16:14:18 +0800

    enable proxy for docker build

--- tools/mock-build-docker/build-container.sh
+++ tools/mock-build-docker/build-container.sh
@@ -3,4 +3,8 @@
 curr_dir=$(readlink -f $(dirname ${0}))
 
 cd $curr_dir
-sudo docker build -t intel-linux-mock-build .
+sudo docker build \
+    --build-arg http_proxy=$http_proxy \
+    --build-arg https_proxy=$https_proxy \
+    --build-arg no_proxy=$no_proxy \
+    -t intel-linux-mock-build .
